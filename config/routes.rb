4f36ab434338a066cdf789813178618cb6f13a63
takuya hakiri <tyapi4@yahoo.co.jp>
2020-01-15 13:06:08 +0900

    add[gem device実装]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'toppage#toppage'
+  devise_for :users, controllers: {
+  sessions: 'users/sessions',
+  registrations: 'users/registrations'
+  }
+
+  # devise_scope :user do
+  # get "user/:id", :to => "users/registrations#detail"
+  # get "signup", :to => "users/registrations#new"
+  # get "login", :to => "users/sessions#new"
+  # get "logout", :to => "users/sessions#destroy"
+  # end
 end
