32442ec964f02860168dfbeea42b347eb735ac86
takuya hakiri <tyapi4@yahoo.co.jp>
2020-02-14 12:10:59 +0900

    add[掲示板に削除機能実装]

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,11 +1,19 @@
 class ResponsesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_board
 
   def show
 
   end
 
   def create
+  end
+
+  def destroy
+    @response = Response.find(params[:id])
+    @response.destroy
+    flash[:success] = '投稿を削除しました'
+    redirect_to board_path(@board)
   end
 
   private
