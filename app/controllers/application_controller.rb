d713c51fb83b6099adb05b7abe26d1eba0beea98
takuya hakiri <tyapi4@yahoo.co.jp>
2020-02-17 21:28:40 +0900

    modified: 環境変数設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   env = Dotenv.load
-  @@api = env['api']
+  @@api = env['API_KEY']
   
   # このアクションを追加
   def after_sign_in_path_for(resource)
