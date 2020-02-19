ba27292ce84b051cb7bb4a24dea0213928dabe7f
takuya hakiri <tyapi4@yahoo.co.jp>
2020-02-19 14:12:46 +0900

    fix[herokuドメイン名を修正]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'lol-frends.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'lol-friends.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
