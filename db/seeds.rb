a6b551dffae9b95cab3b10910f1fc355b5a4fddd
takuya hakiri <tyapi4@yahoo.co.jp>
2020-02-24 11:27:59 +0900

    add[seedの件数を２０件に増加]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-10.times do |i|
+20.times do |i|
 User.create!(name: "test#{i + 1}",
              email: "seed#{i + 1}@example.jp",
              password: "password",
@@ -6,14 +6,14 @@ User.create!(name: "test#{i + 1}",
              confirmed_at: DateTime.now)
 end
 
-5.times do |i|
+20.times do |i|
 Group.create!(name: "group-name#{i + 1 }",
              introduction: "introduction#{i + 1}",
              owner_id: User.find(i + 1).id
              )
 end
 
-5.times do |i|
+20.times do |i|
 Board.create!(title: "title#{i + 1 }",
              content: "content#{i + 1}",
              user_id: User.find(i + 1).id
